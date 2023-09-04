c24705ca4ccf867a2566b7c2f6c048cb3af3bb8d
tauhid92 <tauhidul2011@gmail.com>
2023-09-04 00:28:43 -0400

    basic mod

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,50 @@
+use server::Server;
+
 fn main() {
     let address: String = String::from("127.0.0.1.8080");
 
     let server: Server = Server::new(address);
     server.run();
 }
-struct Server {
-    address : String
-}
 
-impl Server {
-    fn new(address: String) -> Self {
-        Self { address }
+mod server{
+    pub struct Server {
+        address : String
     }
-
-    fn run(self){
-       let port = &self.address[10..];
-        println!("Server listening at port: {}",port);
+    
+    impl Server {
+        pub fn new(address: String) -> Self {
+            Self { address }
+        }
+    
+        pub fn run(self){
+           let port = &self.address[10..];
+            println!("Server listening at port: {}",port);
+        }
     }
 }
 
-struct Request {
-    path: String,
-    query_string: Option<String>,
-    method: Method
+mod http{
+    pub mod request{
+
+        use super::method::Method;
+        struct Request {
+            path: String,
+            query_string: Option<String>,
+            method: Method
+        }
+    }
+    
+    
+    pub mod method{
+        pub enum Method{
+            GET, POST, PUT, DELETE, PATCH, OPTIONS, CONNECT, HEAD, TRACE
+        }
+    }
 }
 
-enum Method{
-    GET, POST, PUT, DELETE, PATCH, OPTIONS, CONNECT, HEAD, TRACE
-}
+
+
 
 /*
 GET /user?id=10 HTTP/1.1\r\n
