b038f111c5ab2461fbbf2aec8404cd663a0bd430
tauhid92 <tauhidul2011@gmail.com>
2023-09-04 22:36:19 -0400

    server runs

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ mod http;
 mod server;
 
 fn main() {
-    let address: String = String::from("127.0.0.1.8080");
+    let address: String = String::from("127.0.0.1:8080".to_string());
 
     let server: Server = Server::new(address);
     server.run();
--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,5 @@
 use std::net::TcpListener;
+use std::io::Read;
 
 pub struct Server {
     address : String
@@ -15,9 +16,16 @@ impl Server {
 
         loop {
             match listener.accept() {
-                Ok((stream, _)) => {
-                    let a = 5;
-                    println!("Ok");
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer){
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                        },
+                        Err(e) => {
+                            println!("Failed to read from connection: {}", e);
+                        }
+                    }
                 },
                 Err(e) => {
                     println!("Failed to establish connection: {}", e);
