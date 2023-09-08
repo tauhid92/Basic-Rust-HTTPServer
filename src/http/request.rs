2c1d943598004583b6c2b2b6ecec24a038d63a5d
tauhid92 <tauhidul2011@gmail.com>
2023-09-07 23:45:31 -0400

    tryfrom intro

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,4 +1,5 @@
 use super::method::Method;
+use std::convert:: TryFrom;
 pub struct Request {
     path: String,
     query_string: Option<String>,
@@ -6,7 +7,17 @@ pub struct Request {
 }
 
 impl Request {
-    pub fn process_request(self) -> String{
-        "".to_string()
+
+    fn from_byte_array(buf: &[u8]) -> Result<Self, String>{
+        unimplemented!()
     }
 }
+
+impl TryFrom<&[u8]> for Request {
+    type Error = String;
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        unimplemented!()
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -10,27 +10,9 @@ mod server;
 fn main() {
     let address: String = String::from("127.0.0.1:8080".to_string());
 
-    // let mut val: String = String::new();
-    // let loc : &String = &val;
-    // println!("{}", *loc);
-    // execute(&mut val);
-    
     let server: Server = Server::new(address);
     server.run();
 }
 
-// fn execute(val: &mut String){
-
-// }
-
-
-
-
-
-/*
-GET /user?id=10 HTTP/1.1\r\n
-HEADERS \r\n
-BODY
-*/
 
 
