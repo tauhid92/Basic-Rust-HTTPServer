cb56102829a920ab2f27a7f1c3fe0bfe742d5fbd
tauhid92 <tauhidul2011@gmail.com>
2023-09-09 14:46:27 -0400

    implemented FromStr for Method

--- src/http/method.rs
+++ src/http/method.rs
@@ -1,3 +1,26 @@
+use std::str::FromStr;
+
 pub enum Method{
     GET, POST, PUT, DELETE, PATCH, OPTIONS, CONNECT, HEAD, TRACE
+}
+
+
+impl FromStr for Method {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "GET" => Ok(Self::GET),
+            "POST" => Ok(Self::POST),
+            "PUT" => Ok(Self::PUT),
+            "DELETE" => Ok(Self::DELETE),
+            "PATCH" => Ok(Self::PATCH),
+            "OPTIONS" => Ok(Self::OPTIONS),
+            "CONNECT" => Ok(Self::CONNECT),
+            "HEAD" => Ok(Self::HEAD),
+            "TRACE" => Ok(Self::TRACE),
+            _ => Err(String::from("error")),
+            
+        }
+    }
 }
