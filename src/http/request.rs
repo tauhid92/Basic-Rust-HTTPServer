0e15dcc2fa517c3af24592cc1467222845e49455
tauhid92 <tauhidul2011@gmail.com>
2023-09-09 12:10:05 -0400

    bla bla

--- src/http/request.rs
+++ src/http/request.rs
@@ -35,6 +35,23 @@ impl TryFrom<&[u8]> for Request {
         unimplemented!()
     }
 }
+
+fn get_next_word(request: &str) -> Option<(&str, &str)> {
+    // unimplemented!()
+    // let mut iter = request.chars();
+    // loop{
+    //     let item = iter.next();
+    //     match item{
+    //         Some(c) => {},
+    //         None => break,
+    //     }
+    // }
+
+    for c in request.chars(){
+
+    }
+}
+
 pub enum ParseError {
     InvalidRequest,
     InvalidProtocol,
