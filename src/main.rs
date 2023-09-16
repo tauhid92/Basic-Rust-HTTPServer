d955e02b421ecdfa7a6d1f6355150d768e76679a
tauhid92 <tauhidul2011@gmail.com>
2023-09-16 02:21:32 -0400

    env

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,9 @@ mod server;
 mod website_handler;
 
 fn main() {
-    
-    let public_path = env::var("PUBLIC_PATH").unwrap();
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
     let server: Server = Server::new("127.0.0.1:8080".to_string());
     server.run(WebsiteHandler::new(public_path));
 }
