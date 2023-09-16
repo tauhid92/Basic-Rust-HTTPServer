b411e16166c6a153aba857691bac0876b906f1e2
tauhid92 <tauhidul2011@gmail.com>
2023-09-16 02:15:04 -0400

    implemented web handler static method

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,11 +1,18 @@
 use super::http::{Request, Response, StatusCode, Method};
 use super::server::Handler;
 
-pub struct WebsiteHandler;
+pub struct WebsiteHandler{
+    public_path: String,
+}
+
+impl WebsiteHandler{
+    pub fn new(public_path: String) -> Self{
+        Self{ public_path}
+    }
+}
 
 impl Handler for WebsiteHandler{
     fn handle_request(&mut self, request: &Request) -> Response {
-        // Response::new(StatusCode::Ok, Some("<h1>Hello!! I am Rust</h1>".to_string()))
         match request.method(){
             Method::GET => match request.path() {
                 "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome!!!</h2>".to_string())),
