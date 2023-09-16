8d4f482cdb3ec34eb9e449a15930948b2cedcc2d
tauhid92 <tauhidul2011@gmail.com>
2023-09-16 02:15:23 -0400

    cleaned up main

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 #![allow(dead_code)]
 use server::Server;
 use website_handler::WebsiteHandler;
+use std::env;
 
 mod http;
 mod server;
 mod website_handler;
 
 fn main() {
-    let address: String = String::from("127.0.0.1:8080".to_string());
-
-    let server: Server = Server::new(address);
-    server.run(WebsiteHandler);
+    
+    let public_path = env::var("PUBLIC_PATH").unwrap();
+    let server: Server = Server::new("127.0.0.1:8080".to_string());
+    server.run(WebsiteHandler::new(public_path));
 }
